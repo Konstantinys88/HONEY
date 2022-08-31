86c6b557dd72f920f5d2e3b0b81d6b08b85a49fd
Konstantin0 <maschkovc@yandex.ru>
2022-08-31 19:15:57 +0300

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -39,4 +39,19 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // слайдер
+    $('.carousel__inner').slick({
+        speed: 300,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/back.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+
 });
